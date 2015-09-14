0b04ad2a2af1954b5eacb05163c13b5a4a437bab
mikemajesty <mikemajesty2011@gmail.com>
2015-09-14 10:19:23 -0300

    Credit

---
+++ Padaria.Model/Entities/Credit.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Padaria.Repository.Entities
+{
+    public class Credit
+    {
+        public int CreditID { get; set; }
+        public int CustomerID { get; set; }
+        public decimal Value { get; set; }
+        public int UserID { get; set; }
+        public virtual Users Users { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
